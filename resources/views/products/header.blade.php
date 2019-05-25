b55d8bca3d501bc487eedf5697f7e8cfaa92f312
C-Shi <cshi@ualberta.ca>
2019-05-26 05:50:31 +0800

    custom sidebar for products

--- resources/views/products/header.blade.php
+++ resources/views/products/header.blade.php
@@ -1,6 +1,7 @@
 @extends('include.header')
 
 @section('stylesheet')
-    <link rel="stylesheet" href="/css/products.css">
-    <link rel="stylesheet" href="/css/checkout.css">
+    <link rel="stylesheet" href="/css/products/products.css">
+    <link rel="stylesheet" href="/css/products/checkout.css">
+    <link rel="stylesheet" href="/css/products/sidebar.css">
 @endsection
--- resources/views/products/sidebar.blade.php
+++ resources/views/products/sidebar.blade.php
@@ -1,8 +1,48 @@
-<div class="card">
-    <ul class="list-group list-group-flush">
-        <li class="list-group-item"><a href="/products">All Products</a></li>
+<div class="side-bar">
+    <ul>
+        <li><a href="/products" class="category">All Products</a></li>
         @foreach ($categories as $category)
-        <li class="list-group-item"><a href="/products?category_id={{$category->id}}">{{$category->name}}</a></li>
+        <li>
+            @switch($category->name)
+                @case('Desktop')
+                    <i class="fas fa-desktop fa-lg"></i>
+                    @break
+                @case('Laptop')
+                    <i class="fas fa-laptop fa-lg"></i>
+                    @break
+                @case('Tablet')
+                    <i class="fas fa-tablet-alt fa-lg"></i>
+                    @break
+                @case('Cellphone')
+                    <i class="fas fa-mobile-alt fa-lg"></i>
+                    @break
+            @endswitch
+            <a href="/products?category_id={{$category->id}}" class="category">{{$category->name}}
+                <small class="right-arrow"></small>
+            </a>
+        </li>
         @endforeach
     </ul>
 </div>
+
+<script>
+    $('.category').mouseover(function(){
+        var cat = $(this);
+        cat.children('.right-arrow').text('>');
+        window.timeout1 = setTimeout(function(){
+            cat.children('.right-arrow').text('>>')
+        }, 100);
+        window.timeout2 = setTimeout(function(){
+            cat.children('.right-arrow').text('>>>')
+        }, 200);
+        window.timeout3 = setTimeout(function(){
+            cat.children('.right-arrow').text('>>>>')
+        }, 300);
+    }).mouseleave(function(){
+        var cat = $(this);
+        cat.children('.right-arrow').text('');
+        clearTimeout(timeout1);
+        clearTimeout(timeout2);
+        clearTimeout(timeout3);
+    })
+</script>
